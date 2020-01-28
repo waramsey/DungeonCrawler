02e5f4096fb167b054fd1d5109f3fa64e603fe75
waramsey <waramsey@unomaha.edu>
2020-01-28 00:37:45 -0600

    Player Movement
    The player can now move!

--- DungeonCrawler/controller.js
+++ DungeonCrawler/controller.js
@@ -2,76 +2,80 @@
 //var socket = io.connect();
 
 //Query DOM
-let map;
-let playerLocation = [10,2]; //right, down
+let canv = document.querySelector('#canv');
+let ctx = canv.getContext('2d');
+
+let map = [['X','X','X','X','X','W','W','W','W','W','W','W','W','W','X','X','X','X','X'],['X','X','X','X','X','W','O','O','O','O','O','O','O','W','X','X','X','X','X'],['X','X','X','X','X','W','O','O','O','O','O','O','O','W','X','X','X','X','X'],['X','X','X','X','X','W','O','O','O','O','O','O','O','W','X','X','X','X','X'],['X','X','X','X','X','W','W','W','O','O','O','W','W','W','X','X','X','X','X'],['X','X','X','X','X','W','O','O','O','O','O','O','O','W','X','X','X','X','X'],['X','X','X','W','W','W','O','W','W','W','W','W','O','W','W','W','X','X','X'],['W','W','W','W','O','O','O','O','O','W','O','O','O','O','O','W','W','W','W'],['W','O','O','W','O','O','O','O','O','W','O','O','O','O','O','W','O','O','W'],
+['W','O','O','O','O','O','O','O','O','W','O','O','O','O','O','O','O','O','W'],
+['W','O','O','W','O','O','O','O','O','W','O','O','O','O','O','W','O','O','W'],
+['W','W','W','W','O','O','O','O','O','W','O','O','O','O','O','W','W','W','W'],
+['X','X','X','W','W','W','O','W','W','W','W','W','O','W','W','W','X','X','X'],
+['X','X','X','X','X','W','O','W','O','O','O','W','O','W','X','X','X','X','X'],
+['X','X','X','X','X','W','O','O','O','O','O','O','O','W','X','X','X','X','X'],
+['X','X','X','X','X','W','W','W','O','O','O','W','W','W','X','X','X','X','X'],
+['X','X','X','X','X','X','X','W','W','W','W','W','X','X','X','X','X','X','X']]
+//Map has to be a 2d array...
+
+let pLoc = [2,12]; //right, down
 
 //Emit events
-MessageChannel.addEventListener('keyup', function(event) {
+document.body.addEventListener('keyup', function(event) {
+    var XorY, moveX = 0, moveY = 0;
     switch (event.keyCode) {
         case 87: case 38: //UP
-            playerLocation[1]--;
+            XorY = 0, moveX = -1;
             break;
         case 65: case 37: //LEFT
-            playerLocation[0]--;
+            XorY = 1, moveY = -1;
             break;
         case 83: case 40: //DOWN
-            playerLocation[1]++;
+            XorY = 0, moveX = 1;
             break;
         case 68: case 39: //RIGHT
-            playerLocation[0]++;
+            XorY = 1, moveY = 1;
             break;
+        default: return;
     }
-    console.log(playerLocation)
+    drawTile(pLoc[0], pLoc[1],'grey','silver')
+    pLoc[XorY] = map[pLoc[0] + moveX][pLoc[1] + moveY] != 'W' ? pLoc[XorY] + moveX + moveY : pLoc[XorY];
+    moveEntity(pLoc[0],pLoc[1],'navy','aqua')
 })
 
 
 
 function draw() {
-    let canv = document.querySelector('#canv');
-    let ctx = canv.getContext('2d');
-    map = ["XXXXXWWWWWWWWWXXXXX","XXXXXWOOOOOOOWXXXXX","XXXXXWOOOOPOOWXXXXX","XXXXXWOOEOOOOWXXXXX","XXXXXWWWOOOWWWXXXXX","XXXXXWOOOOOOOWXXXXX","XXXWWWOWWWWWOWWWXXX","WWWWOOOOOWOOOOOWWWW","WOOWOOOOOWOOOOOWOOW","WOOOOOOOOWOOOOOOOOW","WOOWOOOOOWOOOOOWOOW","WWWWOOOOOWOOOOOWWWW","XXXWWWOWWWWWOWWWXXX","XXXXXWOWOOOWOWXXXXX","XXXXXWOOOOOOOWXXXXX","XXXXXWWWOOOWWWXXXXX","XXXXXXXWWWWWXXXXXXX"];
-
-    ctx.save()
-    ctx.translate(0,0)
-    ctx.scale(1,1)
+    
+    var fill, stroke;
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
-            switch (map[i].charAt(j)) {
+            switch (map[i][j]) {
                 case 'X' :
-                    ctx.fillStyle = 'black'
-                    ctx.strokeStyle = 'black'
+                    fill = 'black'
+                    stroke = 'black'
                     break;
                 case 'O' :
-                    ctx.fillStyle = 'grey'
-                    ctx.strokeStyle = 'silver'
+                    fill = 'grey'
+                    stroke = 'silver'
                     break;
                 case 'W' :
-                    ctx.fillStyle = 'black'
-                    ctx.strokeStyle = 'grey'
+                    fill = 'black'
+                    stroke = 'grey'
                     break;
                 default :
-                    ctx.fillStyle = 'grey'
-                    ctx.strokeStyle = 'silver'
+                    fill = 'grey'
+                    stroke = 'silver'
             }
             
-            ctx.fillRect(j*50,i*50,22,22)
-            ctx.strokeRect(j*50,i*50,22,22)
-            ctx.fillRect(j*50+25,i*50,22,22)
-            ctx.strokeRect(j*50+25,i*50,22,22)
-            ctx.fillRect(j*50,i*50+25,22,22)
-            ctx.strokeRect(j*50,i*50+25,22,22)
-            ctx.fillRect(j*50+25,i*50+25,22,22)
-            ctx.strokeRect(j*50+25,i*50+25,22,22)
+            drawTile(i,j,fill,stroke)
         }
     }
-    ctx.restore()
 
     ctx.save()
     ctx.scale(1,1)
     for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
             let circle = true;
-            switch(map[i].charAt(j)) {
+            switch(map[i][j]) {
                 case 'E' :
                     ctx.fillStyle = 'maroon'
                     ctx.strokeStyle = 'red'
@@ -98,6 +102,38 @@ function draw() {
     ctx.restore()
 }
 
+function moveEntity(x,y,fill,stroke) {
+    ctx.save()
+
+    ctx.fillStyle = fill
+    ctx.strokeStyle = stroke
+    ctx.beginPath()
+    ctx.arc(y*50+23,x*50+23,22,0, Math.PI * 2, true)
+    ctx.stroke()
+    ctx.fill()
+
+    ctx.restore()
+}
+
+function drawTile(x,y,fill,stroke) {
+    ctx.save()
+
+    ctx.clearRect(y*50,x*50,49,49)
+
+    ctx.fillStyle = fill
+    ctx.strokeStyle = stroke
+    ctx.fillRect(y*50,x*50,22,22)
+    ctx.strokeRect(y*50,x*50,22,22)
+    ctx.fillRect(y*50+25,x*50,22,22)
+    ctx.strokeRect(y*50+25,x*50,22,22)
+    ctx.fillRect(y*50,x*50+25,22,22)
+    ctx.strokeRect(y*50,x*50+25,22,22)
+    ctx.fillRect(y*50+25,x*50+25,22,22)
+    ctx.strokeRect(y*50+25,x*50+25,22,22)
+
+    ctx.restore()
+}
+
 function makeMap(mapFile) {
     const fs = require('fs')
 
@@ -109,4 +145,3 @@ function makeMap(mapFile) {
 }
 
 draw();
-
